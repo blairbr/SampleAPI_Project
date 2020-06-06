da3584fda49fcd45566db121ef02aa425e5e95d0
Robert Baxter <baxterrp@gmail.com>
2020-06-05 21:58:41 -0400

    added user client interface

---
+++ PracticeWebApi.Client/Users/IUserClient.cs
@@ -0,0 +1,15 @@
+﻿using PracticeWebApi.CommonClasses.Users;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PracticeWebApi.Client.Users
+{
+    public interface IUserClient
+    { 
+        Task<User> FindUserById(string id);
+        Task<IEnumerable<User>> GetAllUsers();
+        Task RegisterNewUser(User user);
+        Task DeleteUser(string id);
+        Task UpdateUser(User user);
+    }
+}
--- PracticeWebApi.Data/Products/FakeProductRepository.cs
+++ PracticeWebApi.Data/Products/FakeProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using PracticeWebApi.CommonClasses.Exceptions;
+using PracticeWebApi.CommonClasses.Users;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
